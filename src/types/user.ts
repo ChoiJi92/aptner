baf67fa9de2ed4bbfd862d38e42557bd2d644f9f
Choiji92 <jeahoon100@naver.com>
2024-10-24 13:11:39 +0900

    [FIX] getUsers axios를 octokit으로 변경

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,22 +1,21 @@
 export interface UserType {
+  login: string
+  id: number
+  node_id: string
   avatar_url: string
-  events_url: string
+  gravatar_id: string | null
+  url: string
+  html_url: string
   followers_url: string
   following_url: string
   gists_url: string
-  gravatar_id: string
-  html_url: string
-  id: number
-  login: string
-  node_id: string
-  organizations_url: string
-  received_events_url: string
-  repos_url: string
-  score: number
-  site_admin: boolean
   starred_url: string
   subscriptions_url: string
+  organizations_url: string
+  repos_url: string
+  events_url: string
+  received_events_url: string
   type: string
-  url: string
-  user_view_type: string
+  site_admin: boolean
+  score: number
 }
