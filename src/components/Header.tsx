1d1404ed26878592fadb64132c6dbaabe3a6d02d
Choiji92 <jeahoon100@naver.com>
2024-10-24 10:01:19 +0900

    [FEAT] 북마크 기능 구현 및 북마크 페이지 생성

---
+++ src/components/Header.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import styles from './Header.module.scss'
+import SearchIcon from './icons/SearchIcon'
+import BookmarkIcon from './icons/BookmarkIcon'
+
+const Header = () => {
+  const pathname = usePathname()
+
+  return (
+    <header className={styles.header}>
+      <h1>GitHub 사용자 검색</h1>
+      <nav className={styles.nav}>
+        <Link
+          href="/"
+          className={`${styles.navLink} ${pathname === '/' ? styles.active : ''}`}
+        >
+          <SearchIcon />
+          검색
+        </Link>
+        <Link
+          href="/bookmarks"
+          className={`${styles.navLink} ${
+            pathname === '/bookmarks' ? styles.active : ''
+          }`}
+        >
+          <BookmarkIcon bookmarked={false} />
+          북마크
+        </Link>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
--- src/components/User.tsx
+++ src/components/User.tsx
@@ -3,12 +3,25 @@ import Image from 'next/image'
 
 import styles from './User.module.scss'
 import { UserType } from '@/types/user'
+import useUserStore from '@/store/useUserStore'
+import BookmarkIcon from './icons/BookmarkIcon'
 
 const User = ({
   user,
 }: {
   user: Pick<UserType, 'id' | 'login' | 'avatar_url'>
 }) => {
+  const { isBookmarked, addBookmark, removeBookmark } = useUserStore()
+  const bookMarked = isBookmarked(user.id)
+
+  const handleBookmark = () => {
+    if (bookMarked) {
+      removeBookmark(user.id)
+    } else {
+      addBookmark(user)
+    }
+  }
+
   return (
     <div className={styles.container}>
       <Image
@@ -29,8 +42,8 @@ const User = ({
           View Profile
         </Link>
       </div>
-      <button onClick={() => {}} className={styles.bookmarkButton}>
-        북마크
+      <button onClick={handleBookmark} className={styles.bookmarkButton}>
+        <BookmarkIcon bookmarked={bookMarked} />
       </button>
     </div>
   )
